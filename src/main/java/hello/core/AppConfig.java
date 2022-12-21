0de8915dee5b35defaa117d77f20577796e84fc8
dami <wnekfa1004@naver.com>
2022-12-22 01:58:31 +0900

    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,19 +2,35 @@ package hello.core;
 
 import hello.core.Order.OrderService;
 import hello.core.Order.OrderServiceImpl;
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
+/**
+ *  AppConfig 리팩터링
+ *  new MemoryMemberRepository() 부분 중복 제거 이제 MemoryMemberRepository를 다른 구현체로 변경할 때 한 부분만 변경하면된다.
+ *
+ *  AppConfig를 보면 역할과 구현 클래스가 한눈에 들어온다. 애플리케이션 전체 구성이 어떻게 되었는지 빠르게 파악할 수 있다.
+ */
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        // 차후에 메모리가 DB로 바뀌어도 이 코드만 바꾸면됨
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 
