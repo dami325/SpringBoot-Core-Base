6c07545ca29aea090ffed27eedea31ba53a672e3
dami <wnekfa1004@naver.com>
2022-12-22 00:46:48 +0900

    관심사 분리(AppConfig)
    -DI(Dependency Injection) 의존관계 주입(의존성 주입)
    -AppConfig를 통해 관심사 확실하게 분리
    -AppConfig는 공연 기획자다.
    -AppConfig는 구체 클래스를 선택한다. 배역에 맞는 담당 배우를 선택한다. 애플리케이션이 어떻게 동작해야 할지 전체 구성을 책임진다.
    -각 배우는 담당 기능을 실행하는 책임만 지면 된다.

--- src/main/java/hello/core/Order/OrderServiceImpl.java
+++ src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -3,22 +3,21 @@ package hello.core.Order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); // 맴버 정보 리파지토리
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();  // 할인 정책
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();  // 할인 정책
-    private DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 코드를 변경
+    private final MemberRepository memberRepository; // 맴버 정보 리파지토리
+    private final DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 코드를 변경
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
-
-        // 단일 체계 원칙을 잘 지킴 -> 이제 할인 관련을 고치려면 할인쪽만 수정하면 됨.
         int discountPrice = discountPolicy.discount(member, itemPrice);
-        // ----------
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
